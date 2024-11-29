b264087e96d9ae8fbd802863d91f92ba5a1a65f3
hitenkalda <164921592+hitenkalda@users.noreply.github.com>
2024-11-29 21:38:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-nx8i.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-nx8i.onrender.com" : "/api",
   withCredentials: true,
 });
